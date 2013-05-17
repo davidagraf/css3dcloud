0075a5ebff5eaf525d8db025ae69c936c8409a40
David Graf <davidagraf@gmail.com>
2013-05-17 11:25:22 +0000

    wip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+var world = document.getElementById( 'world' ),
+  viewport = document.getElementById( 'viewport' ),
+  
+  d = 0,
+  worldXAngle = 0,
+  worldYAngle = 0;
+
+window.addEventListener( 'mousewheel', onContainerMouseWheel );
+window.addEventListener( 'DOMMouseScroll', onContainerMouseWheel ); 
+
+window.addEventListener( 'mousemove', function( e ) {
+  worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 180;
+  worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 180;
+  updateView();
+} );
+
+function updateView() {
+  var t = 'translateZ( ' + d + 'px ) rotateX( ' + worldXAngle + 'deg) rotateY( ' + worldYAngle + 'deg)';
+  world.style.webkitTransform = t;
+  world.style.MozTransform = t;
+  world.style.oTransform = t;
+}
+
+function onContainerMouseWheel( event ) {
+    
+  event = event ? event : window.event;
+  d = d - ( event.detail ? event.detail * -5 : event.wheelDelta / 8 );
+  updateView();
+  
+}
+
